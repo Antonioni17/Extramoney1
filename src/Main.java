590f25d8ba9bbb04e3cbb331056958d98fcc0ea5
Antonioni17 <antonioni17@yandex.ru>
2023-02-18 15:17:09 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int recentammount = 100;
+        int payment = 900;
+        int bonus = payment/100*1;
+        int finalammount = recentammount + bonus + payment;
+        int nobonusammount = recentammount + payment;
+                if (payment >=1000) {
+                    System.out.print( "Ваш бонус составил : " + bonus +
+                            " Ваш итоговый счет : " + finalammount);
+                } else {System.out.print("Ваш бонус составил : 0 " +
+                "Ваш итоговый счет составил : " + nobonusammount);}
     }
 }
