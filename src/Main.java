d15bfc598d2fb750880b142f95e42e9aa59c4844
Antonioni17 <antonioni17@yandex.ru>
2023-02-26 17:06:31 +0300

    correct version3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int recentammount = 100;
-        int payment = 900;
-        int bonus = payment/100*1;
-        int finalammount = recentammount + bonus + payment;
-        int nobonusammount = recentammount + payment;
-                if (payment >=1000) {
-                    System.out.print( "Ваш бонус составил : " + bonus +
-                            " Ваш итоговый счет : " + finalammount);
-                } else {System.out.print("Ваш бонус составил : 0 " +
-                "Ваш итоговый счет составил : " + nobonusammount);}
+        int recentAmount = 100;
+        int payment = 2100;
+        int bonus = payment / 100 * 1;
+        int finalAmount = recentAmount + bonus + payment;
+        int noBonusAmount = recentAmount + payment;
+        if (payment >= 1000) {
+            System.out.print("Ваш бонус составил : " + bonus +
+                    " Ваш итоговый счет : " + finalAmount);
+        } else {
+            System.out.print("Ваш бонус составил : 0 " +
+                    "Ваш итоговый счет составил : " + noBonusAmount);
+        }
     }
 }
